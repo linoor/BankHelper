c5109801d7311dd510658e59db21657cd004ea62
mpomaran <mpomaran>
2016-01-12 13:50:34 +0100

    finished writing script (working version)

--- bankhelper.hs
+++ bankhelper.hs
@@ -1,5 +1,9 @@
 getLetters :: String -> [Int] -> String
-getLetters pass (x:xs) = 
+getLetters pass nums = map (\(c, n) -> if n `elem` nums then c else '_') $ 
+                       zip pass [1..(length pass)]
 
 main :: IO ()
-main = 
+main = do
+    pass <- putStr "Please enter your password: " >> getLine
+    nums <- putStr "please enter the nums of fields that will be not empty (separated by spaces): " >> map read <$> words <$> getLine
+    putStrLn $ getLetters pass nums
