b17fa8f9de062c6496cdf5ab9bf0fc70120342cb
mpomaran <mpomaran>
2016-01-14 13:39:19 +0100

    the script is working

--- bankhelper.hs
+++ bankhelper.hs
@@ -1,13 +1,15 @@
 import Data.List
 
-getLetters :: String -> [Int] -> [Char]
-getLetters pass nums = map fst $
-                       filter (\(c, n) -> if n `elem` nums then True else False) $ 
-                       zip pass [1..(length pass)]
-
 main :: IO ()
 main = do
     pass <- putStr "Please enter your password: " >> getLine
-    nums <- putStr "please enter the digits of the fields (separated by spaces): " >> map read <$> words <$> getLine
-    putStr "letters in order: "
-    putStrLn $ intersperse ',' $ getLetters pass (sort nums)
+    nums <- putStr "please enter the digits of the fields: " >> map read <$> words <$> getLine
+    let groups = foldr getSequences [] $
+                 -- so that there is no out of bounds exception
+                 map (\n -> n-1) nums where
+        getSequences new [] = [[new]]
+        getSequences new (a:as)
+            | head a - new <= 1 = (new:a):as
+            | otherwise         = [new]:a:as
+    let letters = map (\sublist -> map (\n -> pass !! n) sublist) groups
+    putStrLn $ show letters
